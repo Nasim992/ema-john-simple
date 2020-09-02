d2fd0c714b9929e32a50de1b72b285f8c8d86788
Nasim992 <mdnasim6416@gmail.com>
2020-09-02 07:32:32 +0000

    cart added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,11 +1,27 @@
 
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee,faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 const Products = (props) => {
 
+    console.log(props)
+    const {name,img,seller,price,stock} = props.product;
+
     return (
-        <div>
-            <h4>{props.product.name}</h4>
+        <div className ="product">
+            <div className="product-image">
+                <img src={img}/>
+            </div>
+            <div classNAme="product-details">
+              <h4 className="product-name">{name}</h4> 
+              <br></br>
+              <p><small>by : {seller}</small></p>
+            <p>Price : ${price}</p>
+             <p><small>Only {stock} left in stock -order seller </small></p>
+            <button className="main-button" onClick={()=>props.handleAddProducts(props.product)}><FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+            </div>
+            
         </div>
     );
 };
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -5,17 +5,29 @@ import Products from '../Products/Products';
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products,setProducts] = useState(first10);
+    const [cart,setCart] = useState([])
+
+    const handleAddProducts = (products)=> {
+
+        console.log('products addeded...',products);
+
+        const newCart = [...cart,products];
+
+        setCart(newCart);
+
+    }
     return (
 
         <div className = "shopContainer">
             <div class="productContainer">
                     {
-                        products.map(pd=><Products product={pd}></Products>)
+                        products.map(pd=><Products product={pd} handleAddProducts = {handleAddProducts}></Products>)
                     }
             </div>
             <div className="shoppingCart">
 
                 <h1> This is Cart </h1>
+                <h5>Order Summary : {cart.length}</h5>
 
             </div>
          
